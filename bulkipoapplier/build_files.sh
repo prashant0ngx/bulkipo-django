bada4b2dfb4bab8c086877d2d29dd3305c56d5bd
prashant0ngx <bprashant.kist@gmail.com>
2023-02-20 23:59:14 +0545

    refactor

--- bulkipoapplier/build_files.sh
+++ bulkipoapplier/build_files.sh
@@ -1,2 +1,12 @@
+
+# Path: bulkipoapplier\build_files.sh
+# Description: Build files for bulkipoapplier
+source ipo/scripts/activate
+pip install --upgrade pip
+pip install wheel
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
+python3.9 manage.py createsuperuser
+python3.9 manage.py runserver
